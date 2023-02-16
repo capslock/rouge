cbf2d5d9c2204f63cd477d871e74399eddbad92b
Rich <richard.g.otap@gmail.com>
2023-02-15 18:17:43 -0800

    rouge: add rouge_tracing crate.

---
+++ crates/rouge_tracing/src/sys/native.rs
@@ -0,0 +1,26 @@
+use tracing_subscriber::prelude::*;
+
+pub fn init_tracing() {
+    #[cfg(not(feature = "profile"))]
+    {
+        use tracing::Level;
+        use tracing_subscriber::{filter, FmtSubscriber};
+
+        let filter = filter::filter_fn(|metadata| !metadata.target().starts_with("bevy"));
+        let subscriber = FmtSubscriber::builder()
+            .with_max_level(Level::TRACE)
+            .pretty()
+            .with_target(true)
+            .finish();
+
+        tracing::subscriber::set_global_default(subscriber.with(filter))
+            .expect("setting default subscriber failed");
+    }
+    #[cfg(feature = "profile")]
+    {
+        tracing::subscriber::set_global_default(
+            tracing_subscriber::registry().with(tracing_tracy::TracyLayer::new()),
+        )
+        .expect("set up the subscriber");
+    }
+}
---
+++ crates/rouge_tracing/src/sys/wasm.rs
@@ -0,0 +1,21 @@
+use tracing::Level;
+use tracing_subscriber::fmt::time::UtcTime;
+use tracing_subscriber::{filter, prelude::*, FmtSubscriber};
+use tracing_wasm::{WASMLayer, WASMLayerConfigBuilder};
+
+pub fn init_tracing() {
+    let config = WASMLayerConfigBuilder::default().build();
+    let layer = WASMLayer::new(config);
+
+    let filter = filter::filter_fn(|metadata| !metadata.target().starts_with("bevy"));
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .with_ansi(false)
+        .with_timer(UtcTime::rfc_3339())
+        .pretty()
+        .with_target(true)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber.with(layer).with(filter))
+        .expect("setting default subscriber failed");
+}

2e5158a90380ba73a0d2809bb30328213093264f
Rich <richard.g.otap@gmail.com>
2023-03-14 14:49:48 -0700

    rouge_queue: Add some crate documentaiton.

--- crates/rouge_queue/src/plugin.rs
+++ crates/rouge_queue/src/plugin.rs
@@ -1,15 +1,21 @@
-use bevy::app::Plugin;
+use bevy::{app::Plugin, prelude::World};
 
 use crate::Queue;
 
 type QueueFn = dyn Fn(&mut bevy::app::App) + Send + Sync;
 
+/// Plugin that automatically adds [`Queue`] resources to the app with the types
+/// provided to the [`QueuePlugin::with_queue`] builder.
+///
+/// This plugin can be initialized multiple times, so it can be safely used from
+/// other plugins in the same application.
 #[derive(Default)]
 pub struct QueuePlugin {
     types: Vec<Box<QueueFn>>,
 }
 
 impl QueuePlugin {
+    /// Add a [`Queue`] of type `T` to the application.
     pub fn with_queue<T: Send + 'static>(mut self) -> Self {
         self.types.push(Box::new(|app: &mut bevy::app::App| {
             app.init_resource::<Queue<T>>();
@@ -27,5 +33,23 @@ impl Plugin for QueuePlugin {
 
     fn is_unique(&self) -> bool {
         false
+    }
+}
+
+/// Trait for types that can add a [`Queue`] resource.
+pub trait AddQueue {
+    /// Add a [`Queue`] of type `T` to the `World`.
+    fn add_queue<T: Send + 'static>(&mut self);
+}
+
+impl AddQueue for World {
+    fn add_queue<T: Send + 'static>(&mut self) {
+        self.init_resource::<Queue<T>>();
+    }
+}
+
+impl AddQueue for bevy::prelude::App {
+    fn add_queue<T: Send + 'static>(&mut self) {
+        self.init_resource::<Queue<T>>();
     }
 }
--- crates/rouge_queue/src/queue.rs
+++ crates/rouge_queue/src/queue.rs
@@ -3,6 +3,7 @@ use std::marker::PhantomData;
 use bevy::ecs::{prelude::*, system::SystemParam};
 use crossbeam::queue::SegQueue;
 
+/// A multi-producer multi-consumer queue.
 #[derive(Resource)]
 pub struct Queue<T> {
     q: SegQueue<T>,
@@ -10,37 +11,43 @@ pub struct Queue<T> {
 
 impl<T> Default for Queue<T> {
     fn default() -> Self {
-        Self::new()
-    }
-}
-
-unsafe impl<T: Send> Send for Queue<T> {}
-unsafe impl<T: Send> Sync for Queue<T> {}
-
-impl<T> Queue<T> {
-    pub fn new() -> Self {
         Self {
             q: Default::default(),
         }
     }
+}
 
+impl<T> Queue<T> {
+    // Create a new queue.
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    // Test if the queue is empty.
     pub fn is_empty(&self) -> bool {
         self.q.is_empty()
     }
 
+    // Push an item onto the queue.
     pub fn push(&self, value: T) {
         self.q.push(value)
     }
 
+    // Pop an item off of the queue. Returns `None` if the queue is empty.
     pub fn pop(&self) -> Option<T> {
         self.q.pop()
     }
 
+    // Iterate over items in the queue. This drains the queue, but does not consume
+    // the `Queue` itself.
     pub fn iter(&self) -> QueueIter<T> {
         QueueIter::new(self)
     }
 }
 
+/// An iterator for `Queue`.
+///
+/// Created by calling [`Queue::iter`]. See its documentation for more.
 pub struct QueueIter<'a, T> {
     q: &'a Queue<T>,
 }
@@ -59,12 +66,16 @@ impl<'a, T> Iterator for QueueIter<'a, T> {
     }
 }
 
+/// Push items onto a `Queue`.
+///
+/// Used as a convenience `SystemParam` rather than operating on the underlying
+/// [`Queue`] directly.
 #[derive(SystemParam)]
 pub struct QueueWriter<'w, 's, E>
 where
     E: Send + 'static,
 {
-    pub queue: Res<'w, Queue<E>>,
+    queue: Res<'w, Queue<E>>,
     #[system_param(ignore)]
     marker: PhantomData<&'s ()>,
 }
@@ -73,17 +84,22 @@ impl<'w, 's, E> QueueWriter<'w, 's, E>
 where
     E: Send + 'static,
 {
+    /// Push an item onto the `Queue`.
     pub fn push(&self, value: E) {
         self.queue.push(value)
     }
 }
 
+/// Pop item from the `Queue`.
+///
+/// Used as a convenience `SystemParam` rather than operating on the underlying
+/// [`Queue`] directly.
 #[derive(SystemParam)]
 pub struct QueueReader<'w, 's, E>
 where
     E: Send + 'static,
 {
-    pub queue: Res<'w, Queue<E>>,
+    queue: Res<'w, Queue<E>>,
     #[system_param(ignore)]
     marker: PhantomData<&'s ()>,
 }
@@ -92,23 +108,29 @@ impl<'w, 's, E> QueueReader<'w, 's, E>
 where
     E: Send + 'static,
 {
+    /// Check if the queue is empty without modifying the contents of the `Queue`.
     pub fn is_empty(&self) -> bool {
         self.queue.is_empty()
     }
 
+    /// Pop the top item off of the queue.
     pub fn pop(&self) -> Option<E> {
         self.queue.pop()
     }
 
+    /// Iterate over through items in the `Queue`. This drains the `queue`, but
+    /// does not consume the underlying `Queue`.
     pub fn iter(&self) -> QueueIter<E> {
         self.queue.iter()
     }
 }
 
+/// Trait for pushing items to a [`Queue`].
 pub trait PushQueue<T>
 where
     T: Send + 'static,
 {
+    /// Push an item to the `Queue`.
     fn push_queue(&self, value: T);
 }
 
@@ -116,6 +138,8 @@ impl<T> PushQueue<T> for World
 where
     T: Send + 'static,
 {
+    /// Push an item to the [`Queue`], panicking if the specified `Queue`
+    /// resource does not exist.
     fn push_queue(&self, value: T) {
         match self.get_resource::<Queue<T>>() {
             Some(queue) => queue.push(value),
